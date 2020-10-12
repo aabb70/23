78c8ba125c37276dbc7620ade742f39e4b7e37de
aabb7172 <denis66604@gmail.com>
2020-10-12 23:53:39 +0800

    Add code

---
+++ formapi/views.py
@@ -0,0 +1,55 @@
+from django.conf import settings
+from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseForbidden
+from django.views.decorators.csrf import csrf_exempt
+
+from linebot import LineBotApi, WebhookParser, WebhookHandler
+from linebot.exceptions import InvalidSignatureError, LineBotApiError
+from linebot.models import MessageEvent, TextSendMessage
+from module import func
+from formapi.models import users
+import configparser
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+line_bot_api = LineBotApi(config.get('line-bot', 'channel_access_token'))
+handler = WebhookHandler(config.get('line-bot', 'channel_secret'))
+
+@csrf_exempt
+def callback(request):
+    if request.method == 'POST':
+        signature = request.META['HTTP_X_LINE_SIGNATURE']
+        body = request.body.decode('utf-8')
+        try:
+            events = handler.handle(body, signature)
+        except InvalidSignatureError:
+            return HttpResponseForbidden()
+        except LineBotApiError:
+            return HttpResponseBadRequest()
+
+        for event in events:
+            if isinstance(event, MessageEvent):
+                user_id = event.source.user_id
+                if not (users.objects.filter(uid=user_id).exists()):
+                    unit = users.objects.create(uid=user_id)
+                    unit.save()
+                mtext = event.message.text
+                if mtext[:6] == '123456' and len(mtext) > 6:  #推播給所有顧客
+                    pushMessage(event, mtext)
+    
+        return HttpResponse()
+
+    else:
+        return HttpResponseBadRequest()
+
+def pushMessage(event, mtext):  ##推播訊息給所有顧客
+    try:
+        msg = mtext[6:]  #取得訊息
+        userall = users.objects.all()
+        for user in userall:  #逐一推播
+            message = TextSendMessage(
+                text = msg
+            )
+            line_bot_api.push_message(to=user.uid, messages=[message])  #推播訊息
+    except:
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
