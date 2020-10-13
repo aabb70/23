ac98ebc7b67e0033040130a8ebe11392a7de62e9
aabb7172 <denis66604@gmail.com>
2020-10-14 00:35:31 +0800

    Add code

--- exform/settings.py
+++ exform/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'exform.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',  #PostgreSQL
+        'NAME': 'users',  #資料庫名稱
+        'USER': 'postgres',  #資料庫帳號
+        'PASSWORD': 'aabb7172',  #資料庫密碼
+        'HOST': 'localhost',  #Server(伺服器)位址
+        'PORT': '5432'  #PostgreSQL Port號
     }
 }
 
