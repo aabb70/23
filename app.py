dfeaee1598b7b61aa51769d3d50e42dd19aca29a
aabb7172 <denis66604@gmail.com>
2020-10-18 02:19:11 +0800

    Add code

--- app.py
+++ app.py
@@ -221,7 +221,12 @@ def manageForm(event, text, user_id):  #處理LIFF傳回的FORM資料
         roomtype = flist[0]  #取得輸入資料
         amount = flist[1]
         tel = flist[2]
-        text1 = "親愛的您的問題我們已經收到，個人資料如下："
+        if flist[0] = "男":
+            R = "先生"
+        elif flist[0] = "女":
+            R = "小姐"
+        else R = "用戶"
+        text1 = "親愛的"+ T + R + "您的問題我們已經收到，個人資料如下："
         text1 += "\n性別：" + roomtype
         text1 += "\n姓名：" + amount
         text1 += "\n電話：" + tel
