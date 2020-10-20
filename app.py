e9ff1806618575522e52678dd4dc035b6ee6309e
aabb7172 <denis66604@gmail.com>
2020-10-20 22:07:35 +0800

    Add code

--- app.py
+++ app.py
@@ -278,30 +278,7 @@ def handle_postback(event):
     elif backdata.get('action') == 'Func2':
         sendBack_Func2(event, backdata)
     elif backdata.get('action') == 'Func3':
-        try:
-            message = TemplateSendMessage(
-                alt_text='圖片轉盤樣板',
-                template=ImageCarouselTemplate(
-                    columns=[
-                        ImageCarouselColumn(
-                            image_url='https://i.imgur.com/MZlMaDT.png',
-                            action=PostbackTemplateAction(
-                                label='點擊購買',
-                                data='action=sell&item=麥芽餅&URL=https://reurl.cc/2gK0v4'
-                            )
-                        ),
-                        ImageCarouselColumn(
-                            image_url='https://i.imgur.com/qtOiw17.png',
-                            action=PostbackTemplateAction(
-                                label='點擊購買',
-                                data='action=sell&item=KIN卡碧絲洗髮精&URL=https://reurl.cc/nzRNEv'
-                            )
-                        )
-                    ]
-                )
-            )
-        except:
-            line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
+        sendBack_Func3(event, backdata)
 
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
@@ -421,29 +398,12 @@ def sendBack_Func2(event, backdata):  #處理Postback
         line_bot_api.reply_message(event.reply_token, message)
     except:
         line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
-def sendBack_onsale(event, backdata):
+def sendBack_Func3(event, backdata):  #處理Postback
     try:
-        message = TemplateSendMessage(
-            alt_text='圖片轉盤樣板',
-            template=ImageCarouselTemplate(
-                columns=[
-                    ImageCarouselColumn(
-                        image_url='https://i.imgur.com/MZlMaDT.png',
-                        action=PostbackTemplateAction(
-                            label='點擊購買',
-                            data='action=sell&item=麥芽餅&URL=https://reurl.cc/2gK0v4'
-                        )
-                    ),
-                    ImageCarouselColumn(
-                        image_url='https://i.imgur.com/qtOiw17.png',
-                        action=PostbackTemplateAction(
-                            label='點擊購買',
-                            data='action=sell&item=KIN卡碧絲洗髮精&URL=https://reurl.cc/nzRNEv'
-                        )
-                    )
-                ]
-            )
+        message = TextSendMessage(  #傳送文字
+            text = "@促銷商品"
         )
+        line_bot_api.reply_message(event.reply_token, message)
     except:
         line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
 
